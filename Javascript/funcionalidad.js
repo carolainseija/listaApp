69d100a842707f1d182280b7e06a46d40e8bf500
Carolain <Carolainsilva1@gmail.com>
2020-07-03 01:58:32 -0300

    javascript ultimos detalles

--- Javascript/funcionalidad.js
+++ Javascript/funcionalidad.js
@@ -6,14 +6,36 @@
 
     //funciones
     var comprobarInput = function(){
-       
+       escribirTarea.className = "";
+       escribirTarea.setAttribute("placeholder" , "Agrega tu nueva tarea")
     };
     var agregarTarea = function(){
-        var tarea = escribirTarea.value;
-        alert(tarea);
+        var tarea = escribirTarea.value,
+        nuevaTareaEscrita = document.createElement("li"),
+        enlace = document.createElement("a"),
+        contenido = document.createTextNode(tarea);
+
+        if(tarea == "") {
+            escribirTarea.setAttribute("placeholder", "agregue una tarea valida");
+            escribirTarea.className = "error";
+            return false;
+        }
+// agrego contenido, le agrego enlace, lo pongo dentro de "li"
+        enlace.appendChild(contenido);
+        enlace.setAttribute("href", "#");
+        nuevaTareaEscrita.appendChild(enlace);
+        lista.appendChild(nuevaTareaEscrita);
+//borrar lo que escribiuna vez que se agrego a la lista
+        escribirTarea.value = "";
+     for (var i = 0; i <= lista.children.length -1; i++) {
+        lista.children[i].addEventListener("click", function(){
+            this.parentNode.removeChild(this);
+        });
+     }
     };
+
     var eliminarTarea = function(){
-        
+        this.parentNode.removeChild(this);
     };
 
     //eventos
@@ -23,6 +45,7 @@
 
     for (var i = 0; i <= lista.children.length -1; i++) {
         lista.children[i].addEventListener("click", eliminarTarea);
+    
         
     }
 
